d90f37fdf651f93ad851515d45e014003bb63061
Tibbs39 <kyue3@myseneca.ca>
2018-11-22 16:37:54 -0500

    added student information

--- w9.cpp
+++ w9.cpp
@@ -1,3 +1,11 @@
+// Name:  Kenneth Yue
+// Seneca Student ID: 127932176
+// Seneca email:  kyue3@myseneca.ca
+// Date of completion: Nov 20, 2018
+//
+// I confirm that the content of this file is created by me,
+// with exception of the parts provided to me by my professor
+
 // Workshop 9 - Multi-Threading
 // w9.cpp
 
